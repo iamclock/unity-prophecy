faa36677fd8dd07a839d84ead4cb2c9d8caedc52
Clock <double7@mail.ru>
2018-03-19 16:50:04 +0700

    animation need

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 cameraOffset;
+
+	// Use this for initialization
+	void Start () {
+        cameraOffset = transform.position - target.transform.position;
+//        Debug.Log(target.transform.position.ToString());
+//        Debug.Log(transform.position.ToString());
+	}
+
+	void FixedUpdate () {
+        transform.SetPositionAndRotation( (target.transform.position + cameraOffset), transform.rotation);
+	}
+}
--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 
 
@@ -8,11 +7,19 @@ public class playerController : MonoBehaviour {
 
 	// Use this for initialization
 
+    private AnimationClip moving;
+    public AnimationClip run;
+    public AnimationClip walk;
+    public AnimationClip idle;
+
 	[SerializeField]
 	float moveSpeed = 4f;
 	Vector3 forward, right;
+
 	
 	void Start () {
+//        walk = GetComponent<Animation>("");
+//        run = ;
 		forward = Camera.main.transform.forward;
 		forward.y = 0;
 		forward = Vector3.Normalize(forward);
@@ -23,12 +30,24 @@ public class playerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.anyKey) Move();
-	}
+        else gameObject.GetComponent<Animation>().Play(idle.name);
+    }
 
 
 	void Move(){
+        if( Input.GetButtonDown("SpeedUp") ){
+            moveSpeed = 8f;
+            moving = run;
+            Debug.Log(moveSpeed.ToString());
+		}
+        else {
+			moveSpeed = 4f;
+            moving = walk;
+		}
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
         Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+        gameObject.GetComponent<Animation>().Play(moving.name);
+
 
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
         transform.forward = heading;
